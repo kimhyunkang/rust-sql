cf4614509bd5773d942a4cf6b86a464732e79d0b
kimhyunkang <kimhyunkang@gmail.com>
2014-05-30 18:34:38 +0900

    sqlite3 test code

--- test/test.rs
+++ test/test.rs
@@ -2,7 +2,11 @@
 
 #[phase(syntax)]
 extern crate sql_macro;
+
 extern crate sql;
+extern crate sqlite3;
+
+use sql::adapter::SqlAdapter;
 
 #[sql_table]
 pub struct TestTable {
@@ -13,4 +17,10 @@ pub struct TestTable {
 #[test]
 fn create_table_query_test() {
     assert_eq!(sql::create_table_query::<TestTable>(), "CREATE TABLE IF NOT EXISTS TestTable (a int, b text not null);".to_str())
+}
+
+#[test]
+fn create_table() {
+    let db = sqlite3::open("test.sqlite3").unwrap();
+    // db.create_table_if_not_exists::<TestTable>();
 }
